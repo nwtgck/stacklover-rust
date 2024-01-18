b46062cb2b39d78a7599a35ca10b0dd3f3eeb01c
Ryo Ota <nwtgck@nwtgck.org>
2024-01-18 23:26:25 +0900

    create private struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,56 @@
 #[macro_export]
 macro_rules! stacklover {
     ($struct_name:ident, $n:expr, ( $($param:ident: $param_ty:ty )* ) -> $return_type:ty { $($body:tt)* }) => {
-        struct $struct_name {
-            __private: __PrivateTodo
-        }
+        $crate::private_mod::paste! {
+            struct $struct_name {
+                __private: [<__Stacklover $struct_name>]
+            }
 
-        impl $struct_name {
-            #[inline(always)]
-            pub fn new( $($param: $param_ty )* ) -> Self {
-                Self {
-                    __private: __PrivateTodo {
-                        inner: unsafe {
-                            ::core::mem::transmute::<_, [u8; $n]>(__PrivateTodo::create( $($param)* ))
+            impl $struct_name {
+                #[inline(always)]
+                pub fn new( $($param: $param_ty )* ) -> Self {
+                    Self {
+                        __private: [<__Stacklover $struct_name>] {
+                            inner: unsafe {
+                                ::core::mem::transmute::<_, [u8; $n]>([<__Stacklover $struct_name>]::create( $($param)* ))
+                            },
                         },
-                    },
+                    }
+                }
+
+                pub fn into_entity(self) -> $return_type {
+                    self.__private.entity()
                 }
             }
 
-            // TODO: return type
-            pub fn into_entity(self) -> $return_type {
-                self.__private.entity()
-            }
-        }
-
-        // TODO: name with $struct_name
-        struct __PrivateTodo {
-            inner: [u8; $n],
-        }
-
-        impl __PrivateTodo {
-            #[inline(always)]
-            fn create( $($param: $param_ty )* ) -> $return_type {
-                $($body)*
+            struct [<__Stacklover $struct_name>] {
+                inner: [u8; $n],
             }
 
-            #[inline(always)]
-            fn entity(&self) -> $return_type {
-                if true {
-                    unsafe { ::core::mem::transmute(self.inner) }
-                } else {
-                    #[inline(always)]
-                    fn assert_send_sync_unpin<T: Send + Sync + Unpin>(x: T) -> T {
-                        x
+            impl [<__Stacklover $struct_name>] {
+                #[inline(always)]
+                fn create( $($param: $param_ty )* ) -> $return_type {
+                    $($body)*
+                }
+
+                #[inline(always)]
+                fn entity(&self) -> $return_type {
+                    if true {
+                        unsafe { ::core::mem::transmute(self.inner) }
+                    } else {
+                        #[inline(always)]
+                        fn assert_send_sync_unpin<T: Send + Sync + Unpin>(x: T) -> T {
+                            x
+                        }
+                        #[allow(unreachable_code)]
+                        assert_send_sync_unpin(Self::create(unreachable!()))
                     }
-                    #[allow(unreachable_code)]
-                    assert_send_sync_unpin(Self::create(unreachable!()))
                 }
             }
         }
     };
 }
 
-#[macro_export]
-macro_rules! private_struct_name {
-    ($name:ident) => {
-        paste::paste! {
-            [<__PrivateStacklover $name>]
-        }
-    }
+pub mod private_mod {
+    pub use paste::paste;
 }
