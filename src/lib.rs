d20a04395c8cd04b9dc3aba6d0f33cfe8f38b098
Ryo Ota <nwtgck@nwtgck.org>
2024-01-19 09:25:46 +0900

    feat: async support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_export]
 macro_rules! stacklover {
+    // not async create
     ($struct_name:ident, $n:expr, ( $($param:ident: $param_ty:ty )* ) -> $return_type:ty { $($body:tt)* }) => {
         $crate::private_mod::paste! {
             struct $struct_name {
@@ -38,12 +39,65 @@ macro_rules! stacklover {
                     if true {
                         unsafe { ::core::mem::transmute(self.inner) }
                     } else {
-                        #[inline(always)]
                         fn assert_send_sync_unpin<T: Send + Sync + Unpin>(x: T) -> T {
                             x
                         }
                         #[allow(unreachable_code)]
+                        // TODO: repeat unreachable!()
                         assert_send_sync_unpin(Self::create(unreachable!()))
+                    }
+                }
+            }
+        }
+    };
+    // async create
+    ($struct_name:ident, $n:expr, $async:ident ( $($param:ident: $param_ty:ty )* ) -> $return_type:ty { $($body:tt)* }) => {
+        $crate::private_mod::paste! {
+            struct $struct_name {
+                __private: [<__Stacklover $struct_name>]
+            }
+
+            impl $struct_name {
+                #[inline(always)]
+                pub $async fn new( $($param: $param_ty )* ) -> Self {
+                    Self {
+                        __private: [<__Stacklover $struct_name>] {
+                            inner: unsafe {
+                                ::core::mem::transmute::<_, [u8; $n]>([<__Stacklover $struct_name>]::create( $($param)* ).await)
+                            },
+                        },
+                    }
+                }
+
+                pub fn into_entity(self) -> $return_type {
+                    self.__private.entity()
+                }
+            }
+
+            struct [<__Stacklover $struct_name>] {
+                inner: [u8; $n],
+            }
+
+            impl [<__Stacklover $struct_name>] {
+                #[inline(always)]
+                $async fn create( $($param: $param_ty )* ) -> $return_type {
+                    $($body)*
+                }
+
+                #[inline(always)]
+                fn entity(&self) -> $return_type {
+                    if true {
+                        unsafe { ::core::mem::transmute(self.inner) }
+                    } else {
+                        fn assert_send_sync_unpin<T: Send + Sync + Unpin>(x: T) -> T {
+                            x
+                        }
+                        fn wrap_future<T: core::future::Future<Output = O>, O>(_: T) -> O {
+                            unreachable!()
+                        }
+                        #[allow(unreachable_code)]
+                        // TODO: repeat unreachable!()
+                        assert_send_sync_unpin(wrap_future(Self::create(unreachable!())))
                     }
                 }
             }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,19 +1,39 @@
+use futures::{SinkExt as _, StreamExt as _};
 use stacklover::stacklover;
 
 #[test]
 fn it_works() {
-    stacklover!(IteratorInStack1, 32,
-        (dep1: &'static str) -> impl IntoIterator<Item=i32> {
+    stacklover!(Iterator1, 32,
+        (dep1: &'static str) -> impl Iterator<Item=i32> {
             vec![1, 2, 3, dep1.len() as i32].into_iter().map(|x| x * 2)
         }
     );
 
     let (tx, rx) = std::sync::mpsc::channel();
     std::thread::spawn(move || {
-        let x = IteratorInStack1::new("hello");
-        tx.send(x).unwrap();
+        let iter = Iterator1::new("hello");
+        tx.send(iter).unwrap();
     });
 
     let iter = rx.recv().unwrap().into_entity();
+    assert_eq!(iter.into_iter().collect::<Vec<i32>>(), vec![2, 4, 6, 10]);
+}
+
+#[tokio::test]
+async fn works_with_async() {
+    stacklover!(Iterator2, 32,
+        async (dep1: &'static str) -> impl Iterator<Item=i32> {
+            vec![1, 2, 3, dep1.len() as i32].into_iter().map(|x| x * 2)
+        }
+    );
+
+    let (mut tx, mut rx) = futures::channel::mpsc::channel(1);
+
+    tokio::spawn(async move {
+        let iter = Iterator2::new("hello").await;
+        tx.send(iter).await.unwrap();
+    });
+
+    let iter = rx.next().await.unwrap().into_entity();
     assert_eq!(iter.into_iter().collect::<Vec<i32>>(), vec![2, 4, 6, 10]);
 }
