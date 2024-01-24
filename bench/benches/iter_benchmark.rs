924cec7d1b02c382c73987a3d252cfaedb98ae9e
Ryo Ota <nwtgck@nwtgck.org>
2024-01-25 01:41:21 +0900

    bench: use i64

--- bench/benches/iter_benchmark.rs
+++ bench/benches/iter_benchmark.rs
@@ -1,28 +1,26 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use stacklover::stacklover;
 
-fn iter_i32() -> impl Iterator<Item = i32> {
-    black_box(
-        (1..)
-            .map(|x| x * 3)
-            .take_while(|x| *x < 999999999)
-            .chain("HELLO".chars().map(|c| c as i32).flat_map(|i| [i, i - 65])),
-    )
+fn iter_i64() -> impl Iterator<Item = i64> {
+    black_box(1..)
+        .map(|x| x * 3)
+        .take_while(|x| *x < 999999999)
+        .chain("HELLO".chars().map(|c| c as i64).flat_map(|i| [i, i - 65]))
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("iterator sum");
-    group.bench_function("bare", |b| b.iter(|| iter_i32().sum::<i32>()));
-    group.bench_function("boxed", |b| b.iter(|| Box::new(iter_i32()).sum::<i32>()));
+    group.bench_function("bare", |b| b.iter(|| iter_i64().sum::<i64>()));
+    group.bench_function("boxed", |b| b.iter(|| Box::new(iter_i64()).sum::<i64>()));
     group.bench_function("stacklover", |b| {
         stacklover! {
             // struct name to be defined
             Iterator1,
-            fn () -> impl Iterator<Item=i32> {
-                iter_i32()
+            fn () -> impl Iterator<Item=i64> {
+                iter_i64()
             }
         }
-        b.iter(|| Iterator1::new().into_inner().sum::<i32>())
+        b.iter(|| Iterator1::new().into_inner().sum::<i64>())
     });
     group.finish();
 }
